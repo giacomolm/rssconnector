f08e83468b222cdd439d816b9e31cbe092f9546e
giacomolm <giacomolm@b2698f5f-bf0e-e037-6940-89b07525da4f>
2011-11-03 14:16:18 +0000

    dalla biblioteca

--- trunk/RssConnector/src/Connector/Test/WriteFeedbackTest.java
+++ trunk/RssConnector/src/Connector/Test/WriteFeedbackTest.java
@@ -3,6 +3,7 @@ package Connector.Test;
 import static org.junit.Assert.*;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.Iterator;
 
 import org.junit.Test;
@@ -17,13 +18,14 @@ public class WriteFeedbackTest {
 	
 	@Test
 	public void testWriteFeedback(){
-		Post p = new Post(0, "titolo", "http://link.it", "description", null, null, null, null, null, null);
+		int ts = (int)(new Date()).getTime();
+		Post p = new Post(0, "titolo", "http://link.it", "description"+ts, null, null, null, null, null, null);
 		RssWriter w = new RssWriter("http://atlantis.isti.cnr.it:8080/virtualNoticeBoard/", "alias", "author");
 		boolean res = false;
 		RssReader r = new RssReader("http://atlantis.isti.cnr.it:8080/virtualNoticeBoard/", "");
 		w.writePost(p, r);
 		long feedbackname = r.findFeedbackName(p);
-		Feedback f = new Feedback("descrizione", Title.AGREE, feedbackname);
+		Feedback f = new Feedback("descrizione"+ts, Title.AGREE, feedbackname);
 		f.setAuthor("Tester");
 		w.writeFeedback(f);
 		Collection<Feedback> set =  r.readFeedbacks(feedbackname);
